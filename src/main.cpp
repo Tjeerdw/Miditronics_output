3cb454a9c5bbfb81f43fb933ef0c285d9dc6419d
Tjeerd Weijers <t.weijers@uva.nl>
2020-12-10 23:12:38 +0100

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,26 @@ TwoWire display_I2C =  TwoWire(0);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &display_I2C, OLED_RESET);
 
 void setup() {
-  Serial.begin(115200);\
+  //USB serial init
+  Serial.begin(115200);
 
+  //display init
   display_I2C.begin(DISPLAY_I2C_SDA, DISPLAY_I2C_SCL, 100000);
-
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println("SSD1306 allocation failed");
     for(;;); // Don't proceed, loop forever
   } 
-  display.clearDisplay();
 
+  //first little text test
+  display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // Display static text
+  display.setCursor(30, 10);
   display.println("Hello     Church!");
   display.display(); 
 }
 
 void loop() {
-  
-  Serial.println("test");
+  Serial.println("loop");
   delay(1000);
 }
