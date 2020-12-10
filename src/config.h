f327ee9e1b4ee957c54a737095ec2800ed559a7c
Tjeerd Weijers <t.weijers@uva.nl>
2020-12-10 23:03:47 +0100

    hello church!

--- src/config.h
+++ src/config.h
@@ -8,3 +8,9 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1
+
+//I2C
+#define DISPLAY_I2C_SDA 25
+#define DISPLAY_I2C_SCL 26
+#define EXTENDERS_I2C_SDA 21
+#define EXTENDERS_I2C_SCL 22
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,30 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+TwoWire display_I2C =  TwoWire(0);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &display_I2C, OLED_RESET);
 
 void setup() {
-  Serial.begin(115200);
-  
+  Serial.begin(115200);\
+
+  display_I2C.begin(DISPLAY_I2C_SDA, DISPLAY_I2C_SCL, 100000);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;); // Don't proceed, loop forever
+  } 
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello     Church!");
+  display.display(); 
 }
 
 void loop() {
+  
   Serial.println("test");
   delay(1000);
 }
