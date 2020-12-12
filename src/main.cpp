05fc8f3646a229d91be743d29b55cab2ae894e80
Tjeerd Weijers <t.weijers@uva.nl>
2020-12-12 13:22:54 +0100

    working i2c scan on extender bus

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <Adafruit_SSD1306.h>
 
 TwoWire display_I2C =  TwoWire(0);
+TwoWire extenders_I2C =  TwoWire(1);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &display_I2C, OLED_RESET);
 
 void setup() {
@@ -20,6 +21,10 @@ void setup() {
     for(;;); // Don't proceed, loop forever
   } 
 
+  //extender init
+  extenders_I2C.begin(EXTENDERS_I2C_SDA, EXTENDERS_I2C_SCL, 100000);
+
+  
   //first little text test
   display.clearDisplay();
   display.setTextSize(2);
@@ -30,6 +35,37 @@ void setup() {
 }
 
 void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    extenders_I2C.beginTransmission(address);
+    error = extenders_I2C.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+
+
   Serial.println("loop");
   delay(1000);
 }
