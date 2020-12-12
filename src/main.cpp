a9fb17076949eac4f00c09356b8e4d8bb6517b63
Tjeerd-CB <tjeerdweijers@gmail.com>
2020-12-12 17:58:15 +0100

    git  test and code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-
 TwoWire display_I2C =  TwoWire(0);
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &display_I2C, OLED_RESET);
 
 void setup() {
@@ -33,14 +31,11 @@ void setup() {
   display.setCursor(30, 10);
   display.println("Hello     Church!");
   display.display(); 
-
-  
 }
 
 void loop() {
   setOutput(17,1);
   delay(1000);
-
   setOutput(17,0);
   delay(1000);
 }
