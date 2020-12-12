d8f7851bc078ef31c54d6f6d4f0489c0dfc0f875
Tjeerd Weijers <t.weijers@uva.nl>
2020-12-12 14:02:17 +0100

    fixed config.h endif

--- src/config.h
+++ src/config.h
@@ -1,22 +1,23 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
-#endif
 
 #include <Arduino.h>
 
 //OLED
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1
+#define OLED_RESET    -1
 
 //I2C
-#define DISPLAY_I2C_SDA 25
-#define DISPLAY_I2C_SCL 26
-#define EXTENDERS_I2C_SDA 21
-#define EXTENDERS_I2C_SCL 22
+#define DISPLAY_I2C_SDA     25
+#define DISPLAY_I2C_SCL     26
+#define EXTENDERS_I2C_SDA   21
+#define EXTENDERS_I2C_SCL   22
 
 //Extenders
 #define ADDRESS_EXT1 0
 #define ADDRESS_EXT2 1
 #define ADDRESS_EXT3 2
 #define ADDRESS_EXT4 3
+
+#endif
