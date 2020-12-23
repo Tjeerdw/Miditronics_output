f4f4dbbc6d93940bbeea0380d189381093454e24
Tjeerd Weijers <t.weijers@uva.nl>
2020-12-23 19:41:41 +0100

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,13 @@
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, MIDI);
 //config (temp) variables for midi implementation, some are waiting on menu implementation and eeprom storage
 int listeningMidiChannel=1;
-boolean registerModule = false;
+boolean registerModule = false; // if not register, it must be notenmodule
 int controlChangeAan = 80; //control change waarde aan
 int controlChangeUit = 81; //control change waarde uit
 int totaalModuleKanalen = 64; //definieert het aantal kanalen dat deze module kan aansturen (32/64)
 int registerOffSet = 0; //registeroffset in geval van extra registermodule
 int startNoot = 36; //midi-nootnummer waarop deze module moet starten (23 = C1, 36 = C2, 49 = C3)
 int eindNoot = (startNoot + totaalModuleKanalen); //midi-nootnummer waar deze module stopt met reageren
-
 
 using namespace Menu;
 
@@ -54,7 +53,6 @@ TOGGLE(ledCtrl,setOutputType,"type: ",doNothing,noEvent,noStyle//,doExit,enterEv
   ,VALUE("Registers",LOW,doNothing,noEvent)
 );
 
-
 MENU(mainMenu,"--------Menu---------",doNothing,noEvent,wrapStyle
   ,FIELD(listeningMidiChannel,"Channel","",1,16,1,0,doNothing,noEvent,wrapStyle)
   ,SUBMENU(setOutputType)
@@ -79,7 +77,7 @@ MENU_OUTPUTS(out,MAX_DEPTH
 
 NAVROOT(nav,mainMenu,MAX_DEPTH,joystickBtns,out);
 
-//excecute when menu exit
+//excecute on menu exit
 result idle(menuOut& o,idleEvent e) {
   o.setCursor(0,0);
   o.print(F("Miditronics Output"));
@@ -89,8 +87,7 @@ result idle(menuOut& o,idleEvent e) {
 }
 
 //note-On message afhandelen
-void handleNoteOn(byte incomingChannel, byte pitch, byte velocity)
-{
+void handleNoteOn(byte incomingChannel, byte pitch, byte velocity){
   if (!registerModule) {  
     velocity = 127; //ter ere van Hendrikus
     pitch = (pitch-(startNoot-1)); //converteert noot naar het juiste outputnummer        
@@ -99,8 +96,7 @@ void handleNoteOn(byte incomingChannel, byte pitch, byte velocity)
 }
 
 //note-Off message afhandelen
-void handleNoteOff(byte incomingChannel, byte pitch, byte velocity)
-{
+void handleNoteOff(byte incomingChannel, byte pitch, byte velocity){
   if (!registerModule) {
     velocity = 127; //ter ere van Hendrikus
     if ((pitch>=startNoot) && (pitch<=eindNoot)) {
@@ -111,8 +107,7 @@ void handleNoteOff(byte incomingChannel, byte pitch, byte velocity)
 }
 
 //Control-Change message afhandelen
-void handleControlChange(byte incomingChannel, byte incomingNumber, byte incomingValue)
-{
+void handleControlChange(byte incomingChannel, byte incomingNumber, byte incomingValue){
   if (registerModule) {
   //Generaal Reset
     if ((incomingValue == 127) && (incomingNumber == controlChangeUit)) {
@@ -147,7 +142,6 @@ void handleControlChange(byte incomingChannel, byte incomingNumber, byte incomin
   }   
 }
 
-
 void setup() {
   //display init
   display_I2C.begin(DISPLAY_I2C_SDA, DISPLAY_I2C_SCL, 100000);
@@ -158,8 +152,6 @@ void setup() {
 
   //first little text test
   display.clearDisplay();
-  //display.setFont(&FreeMono9pt7b);
-  //display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
   display.println("Miditronics Output");
@@ -192,7 +184,7 @@ void setup() {
   MIDI.setHandleControlChange(handleControlChange);
   MIDI.begin(listeningMidiChannel); //luister op opgegeven kanaal
 
- display.println("Starting...");
+  display.println("Starting...");
   display.display();  
   delay(2000);
 
