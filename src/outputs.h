fc4f897b2a22c70223ada5a7aae10f83c0c6ee58
Tjeerd Weijers <t.weijers@uva.nl>
2021-02-14 23:24:57 +0100

    added bitToGPIO funciton

--- src/outputs.h
+++ src/outputs.h
@@ -8,5 +8,6 @@ void extendersInit(uint8_t channelNumbers, uint8_t isThisAnOutputModule);
 uint8_t extendersCount();
 void setOutput(uint8_t outputNumber, uint8_t outputValue);
 void readInputs(uint8_t channelNumbers, uint16_t inputbuffertje[]);
+uint8_t bitToGPIO(uint8_t bit);
 
 #endif
